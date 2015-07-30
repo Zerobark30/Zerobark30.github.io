851709161e190e5c6f9670128362e4ba78550ac7
Matthew Wells <matthewpwells@gmail.com>
2015-07-30 09:17:10 -0700

    Change picture rollover to every hour

--- js/app.js
+++ js/app.js
@@ -1,12 +1,6 @@
 today = new Date();
-dayCount = (Math.floor(today/8.64e7) - 16644);
-function imgCount(e) {
-	if(e > 104)
-		return e - 104;
-	else
-		return e;
-};
-imgNum = imgCount(dayCount);
+hourCount = (Math.floor(today/(3.6e6)) - 399500);
+imgNum = hourCount % 104;
 console.log(imgNum);
 imgSrc = "images/IMG_" + imgNum + ".JPG";
 document.getElementById('image').src=imgSrc;
